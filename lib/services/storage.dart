a5afec7b5cd199ab2f15b268ad18054a9bf346fd
Aqudi <gjdigj145@gmail.com>
2021-07-04 21:51:10 +0900

    feat: Create storage service for reading text file

---
+++ lib/services/storage.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/services.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final storageServiceProvider = Provider<StorageService>(
+  (ref) => StorageService(),
+);
+
+class StorageService {
+  final utf8Decoder = Utf8Decoder();
+
+  /// Pick and return txt file
+  Future<String?> pickAndReadTextFile() async {
+    try {
+      final result = await FilePicker.platform.pickFiles(
+        type: FileType.custom,
+        allowedExtensions: ['txt'],
+      );
+      if (result != null && result.files.first.bytes != null) {
+        final bytes = result.files.first.bytes;
+        return utf8Decoder.convert(bytes!);
+      }
+    } on PlatformException catch (e) {
+      throw Exception('Unsupported operation' + e.toString());
+    } catch (ex) {
+      throw Exception(ex);
+    }
+  }
+}
