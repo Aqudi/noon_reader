27908d38bdfa3b27d5463d5360361127cf2fa3ed
Aqudi <gjdigj145@gmail.com>
2021-07-07 02:54:49 +0900

    feat: Apply setting service and scroll

--- lib/pages/viewer/viewer.dart
+++ lib/pages/viewer/viewer.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:noon_reader/services/setting.dart';
 
-class ViewerPage extends StatelessWidget {
-  final String _content;
-  ViewerPage({required String content}) : _content = content;
+class ViewerPage extends HookWidget {
+  final String content;
+
+  ViewerPage({required this.content});
 
   @override
   Widget build(BuildContext context) {
+    final setting = useProvider(settingServiceProvider).setting;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Noon Reader'),
         centerTitle: true,
       ),
-      body: Text(_content),
+      body: Container(
+        height: double.infinity,
+        color: setting.backgroundColor,
+        child: SingleChildScrollView(
+          padding: setting.padding,
+          child: Text(
+            content,
+            style: Theme.of(context).textTheme.bodyText1,
+          ),
+        ),
+      ),
     );
   }
 }
