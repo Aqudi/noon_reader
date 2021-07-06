0c357d8526bdb27a793e6b1f7bcdf019a51d5bb4
Aqudi <gjdigj145@gmail.com>
2021-07-07 00:42:19 +0900

    feat: Create app constants file

---
+++ lib/constants/app.dart
@@ -0,0 +1,14 @@
+class AppConstants {
+  static const appName = 'Noon Reader';
+
+  static const defaultLanguage = 'English';
+  static const languages = ['English'];
+
+  static const defaultFontFamily = 'Nanum Gothic';
+  static const fontFamilies = [
+    'Dokdo',
+    'Nanum Gothic',
+    'Nanum Myeongjo',
+    'Nanum Pen Script',
+  ];
+}
